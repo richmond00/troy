3e1df6ea349a3a1cc7792d7a23fcd39761ee36e0
Dongwook Kim <dongwook227@gmail.com>
2023-01-29 18:10:08 -0500

    updated scaffolding code

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,18 +1,13 @@
-import { UserList } from './views/UserList';
-import { Collection } from './models/Collection';
-import { UserProps, User } from './models/User';
+import { UserEdit } from "./views/UserEdit";
+import { User } from "./models/User";
 
-const users = new Collection("http://localhost:3000/uesrs",
-    (json: UserProps) => {
-    return User.buildUser(json);
-    });
+const user = User.buildUser({ name: 'NAME', age: 20 });
+const root = document.getElementById('root');
 
-users.on('change', () => {
-    const root = document.getElementById('root');
-
-    if (root) {
-        new UserList(root, users).render();
-    }
-});
-
-users.fetch();
+if (root) {
+    const userEdit = new UserEdit(root, user);
+    userEdit.render();
+    console.log(userEdit);
+} else {
+    throw new Error('Root element not found');
+}
